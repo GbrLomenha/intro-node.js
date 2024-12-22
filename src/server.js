10b76d7a76c1a5629a91947d7ef774a46aa4129f
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2024-12-22 11:40:20 -0300

    criação do model User e rotas de criar e listar

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+prisma.$connect(); // Conectar ao banco de dados
 
 const app = express();
+app.use(express.json()); // Para o express entender JSON
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    console.log(req);
+    return res.status(200).json({ message: 'Hello World' });
+})
+
+app.post('/usuario/cadastrar', async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    return res.status(201).json(await prisma.user.create({
+        data: {
+            name: data.name,
+            email: data.email,
+        }
+    }));
+})
+
+app.get('/usuario/listar', async (req, res) => {
+    const usuarios = await prisma.user.findMany();
+    return res.status(200).json(usuarios);
 })
 
 app.listen(3000, () => {
